248533970deca339c27f7651954eb6eaacad48b1
Anderson Alves <andersoonalvess@gmail.com>
2020-04-16 00:21:56 -0300

    [ReactNative] List repositories and like repositories funcions ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+/**
+ * iOS with Emulator: localhost
+ * iOS with phone: IP of my machine 
+ * Android with emulator: localhost (adb reverse tcp:xxxx (port of my machine) tcp:xxxx (emulator port))
+ * Android with emulator: 10.0.2.2 (Android Studio) 
+ * Android with emulator: 10.0.3.2 (Genymotion) 
+ * Android with phone: IP of my machine
+ */
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://10.0.2.2:3333",
 });
 
 export default api;
